bd959bdfaf8db3cad3934d9386e58c163822699c
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-05-22 10:57:25 -0400

    Adds DoubleExists.py

---
+++ DoubleExists.py
@@ -0,0 +1,15 @@
+"""
+1346. Check If N and Its Double Exist
+
+"""
+
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+
+        for i in range(len(arr)):
+            double = arr[i]*2
+            if double in arr and arr.index(double) != i:
+                return True
+            
+        return False
+                
